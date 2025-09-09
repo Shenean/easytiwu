948d99047e478402388b3065bf7d05e4416e7c06
sheny <shenyankm@gmail.com>
2025-09-09 22:35:17 +0800

    push

---
+++ service-bank/src/main/java/com/easytiwu/servicebank/mapper/QuestionBankMapper.java
@@ -0,0 +1,12 @@
+package com.easytiwu.servicebank.mapper;
+
+import com.easytiwu.servicebank.entity.QuestionBank;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author sheny
+ */
+@Mapper
+public interface QuestionBankMapper extends BaseMapper<QuestionBank> {
+}
