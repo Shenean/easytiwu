b300e123bdbce261803b956ed2ff40309b44937c
sheny <shenyankm@gmail.com>
2025-09-23 15:19:06 +0800

    refactor(mapper): 移除未使用的导入和方法
    - 删除 QuestionMapper 中未使用的 @Param 注解和 List 导入- 移除 QuestionMapper 接口中未使用的 selectByBankIds 方法
    - 删除 QuestionOptionMapper 中未使用的 @Param 注解和 List 导入
    - 移除 QuestionOptionMapper 接口中未使用的 selectByQuestionIds 方法

--- service-bank/src/main/java/com/easytiwu/servicebank/mapper/QuestionOptionMapper.java
+++ service-bank/src/main/java/com/easytiwu/servicebank/mapper/QuestionOptionMapper.java
@@ -3,14 +3,10 @@ package com.easytiwu.servicebank.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.easytiwu.servicebank.entity.QuestionOption;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 /**
  * @author sheny
  */
 @Mapper
 public interface QuestionOptionMapper extends BaseMapper<QuestionOption> {
-    List<QuestionOption> selectByQuestionIds(@Param("questionIds") List<Long> questionIds);
 }
