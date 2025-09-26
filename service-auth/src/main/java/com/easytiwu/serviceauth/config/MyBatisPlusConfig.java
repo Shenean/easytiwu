e1b7db234bcfb35568918aea9e05a6b21e1dedf6
sheny <shenyankm@gmail.com>
2025-09-26 13:26:06 +0800

    docs(config):为MyBatisPlusConfig类添加作者注释
    - 在MyBatisPlusConfig类上添加了@author sheny注释- 用于标识该配置类的创建者信息
    -便于后续维护和问题追踪

--- service-auth/src/main/java/com/easytiwu/serviceauth/config/MyBatisPlusConfig.java
+++ service-auth/src/main/java/com/easytiwu/serviceauth/config/MyBatisPlusConfig.java
@@ -2,6 +2,9 @@ package com.easytiwu.serviceauth.config;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author sheny
+ */
 @Configuration
 public class MyBatisPlusConfig {
     
